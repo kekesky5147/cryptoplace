6c3e9bfdb1b4f6365c6134b0af764f76cfbf3b9f
Minseok Kim^C <ksj5147@gmail.com^C>
2024-08-31 14:27:02 -0700

    crypto_place

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/NAvbar/Navbar"
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home/Home"
 import Coin from "./pages/Coin/Coin"
+import Footer from "./components/Footer/Footer"
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/coin/:coinId" element={<Coin />} />
       </Routes>
+      <Footer />
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,20 @@ import { CoinContext } from "../../context/CoinContext"
 const Home = () => {
   const { allCoin, currency } = useContext(CoinContext)
   const [displayCoin, setDisplayCoin] = useState([])
+  const [input, setInput] = useState("")
+
+  // 하위 검색어가 포함된 검색결과 표시하는 기능 ex)bitcoin의 bit만 검색해도 bit를 포함한 모든 리스트 표시
+  const inputHandler = (event) => {
+    setInput(event.target.value)
+  }
+  const searchHandler = async (event) => {
+    event.preventDefault()
+    const coins = await allCoin.filter((item) => {
+      return item.name.toLowerCase().includes(input.toLowerCase())
+    })
+    setDisplayCoin(coins)
+  }
+  //////////////////////////////////////////////////
 
   useEffect(() => {
     setDisplayCoin(allCoin)
@@ -19,8 +33,14 @@ const Home = () => {
           <br /> Crypto Market
         </h1>
         <p>made by Minseok</p>
-        <form>
-          <input type="text" placeholder="search crypto" />
+        <form onSubmit={searchHandler}>
+          <input
+            onChange={inputHandler}
+            value={input}
+            type="text"
+            placeholder="search crypto"
+            required
+          />
           <button type="submit">Search</button>
         </form>
       </div>
@@ -35,14 +55,18 @@ const Home = () => {
         {displayCoin.slice(0, 10).map((item, index) => (
           <div className="table-layout" key={index}>
             <p>{item.market_cap_rank}</p>
-            <div>
+            <div className="line">
               <img src={item.image} alt="" />
               <p>{item.name + " - " + item.symbol}</p>
             </div>
             <p>
               {currency.symbol} {item.current_price}
             </p>
-            <p>{Math.floor(item.price_change_percentage_24h * 100) / 100}</p>
+            <p
+              className={item.price_change_percentage_24h > 0 ? "green" : "red"}
+            >
+              {Math.floor(item.price_change_percentage_24h * 100) / 100}
+            </p>
             <p className="market-cap">
               {currency.symbol}
               {item.market_cap.toLocaleString()}
