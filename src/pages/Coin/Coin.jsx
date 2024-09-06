412c63bf99d7067b17e155f11bf107122fb089f9
Minseok Kim^C <ksj5147@gmail.com^C>
2024-09-06 14:55:22 -0700

    crypto_place

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -1,13 +1,72 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react"
+import React, { useContext, useEffect, useState } from "react"
 import "./Coin.css"
+import { useParams } from "react-router-dom"
+import { CoinContext } from "../../context/CoinContext"
 
 const Coin = () => {
-  return (
-    <div>
-      <h2>Coin</h2>
-    </div>
-  )
+  const { coinId } = useParams()
+  const [coinData, setCoinData] = useState()
+  const [historicalData, setHistoricalData] = useState()
+  const { currency } = useContext(CoinContext)
+
+  const fetchCoinData = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": "CG-yWj3fU2qiLoXUEN6Si4Lo3yJ	",
+      },
+    }
+
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options)
+      .then((response) => response.json())
+      .then((response) => setCoinData(response))
+      .catch((err) => console.error(err))
+  }
+
+  const fetchHistoricalData = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": "CG-yWj3fU2qiLoXUEN6Si4Lo3yJ	",
+      },
+    }
+
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setHistoricalData(response))
+      .catch((err) => console.error(err))
+  }
+
+  useEffect(() => {
+    fetchCoinData()
+  }, [currency])
+
+  if (coinData && historicalData) {
+    return (
+      <div className="coin">
+        <div className="coin-name">
+          <img src={coinData.image.large} alt="" />
+          <p>
+            <b>
+              {coinData.name} ({coinData.symbol.toUpperCase()})
+            </b>
+          </p>
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div className="spinner">
+        <div className="spin"></div>
+      </div>
+    )
+  }
 }
 
 export default Coin
